8d1e8d4998c8045dc9bc606b21760ca106d0f885
Kiran <smkiran.09@gmail.com>
2024-04-20 08:54:29 +0530

    completed fetching data for test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import { Button } from "react-bootstrap/Button";
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://datausa.io/api/data?drilldowns=Nation&measures=Population"
+        );
+        const jsonData = await response.json();
+
+        setData(jsonData.data);
+      } catch (error) {
+        console.error("Not able to fetch", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <h1>Calling API</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nation</th>
+            <th>Population</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.Nation}</td>
+                <td>{item.Population}</td>
+                <td>{item.Year}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
   );
 }
